43651f1ef92b3068498ee89a55c4155ce4681756
canyon <ravinsdrive@gmail.com>
2018-06-02 21:36:01 -0700

    Adding tmux initializer

---
+++ dev/blog.sh
@@ -0,0 +1,20 @@
+# !/bin/sh
+# Small script that sets up blogging environment with one command
+
+# Kill open session
+tmux kill-session -t blog || true
+
+# Create new session
+tmux new-session -d -s blog 
+
+# C-m is carriage return in tmux
+tmux send-keys 'source activate blog && pelican -r -s pelicanconf.py' 'C-m' 
+
+# Start http server
+tmux select-window -t blog:0
+tmux split-window -v 
+tmux send-keys 'cd .. && python -m http.server' 'C-m'
+
+# Ready to blog
+tmux new-window -n blog_article -c $PWD"/content"
+tmux att -t blog
