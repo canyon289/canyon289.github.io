868c51caae1aece2dafd4f3ff21c9273bf64fefd
Ravin Kumar <ravinsdrive@gmail.com>
2021-10-06 17:21:21 -0700

    Enable https

--- dev/pelicanconf.py
+++ dev/pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Ravin Kumar'
 SITENAME = ['Ravin Kumar']
-SITEURL = 'http://canyon289.github.io'
+SITEURL = 'https://canyon289.github.io'
 META_DESCRIPTION = 'Ravin Kumar is a data scientist and an open source contributor.'
 
 # Content Path
